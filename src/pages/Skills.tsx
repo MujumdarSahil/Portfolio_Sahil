06f416542cedbffac66fe340d4593d69824f118e
MujumdarSahil <your_github_email@example.com>
2026-06-03 15:51:33 +0530

    bug fixed

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './Skills.css';
 import { getSkills } from '../queries/getSkills';
 
-import { FaReact, FaNodeJs, FaAws, FaDocker, FaGitAlt, FaJava, FaPython, FaShieldAlt, FaDatabase, FaChartLine, FaLock, FaEye, FaNetworkWired } from 'react-icons/fa';
-import { SiRubyonrails, SiTypescript, SiPostgresql, SiMysql, SiKubernetes, SiGooglecloud, SiSpringboot, SiPhp, SiNetlify, SiHeroku, SiHtml5, SiCss3, SiRabbitmq, SiImessage, SiTensorflow, SiPytorch, SiJupyter, SiPandas, SiNumpy, SiScikitlearn, SiKalilinux, SiWireshark, SiMetasploit, SiBurpsuite } from 'react-icons/si';
+import { FaReact, FaNodeJs, FaAws, FaDocker, FaJava, FaPython, FaShieldAlt, FaDatabase, FaChartLine, FaLock, FaEye, FaNetworkWired } from 'react-icons/fa';
+import { SiRubyonrails, SiTypescript, SiPostgresql, SiMysql, SiKubernetes, SiGooglecloud, SiSpringboot, SiPhp, SiNetlify, SiHeroku, SiRabbitmq, SiImessage, SiTensorflow, SiPytorch, SiJupyter, SiPandas, SiNumpy, SiScikitlearn, SiKalilinux, SiWireshark, SiMetasploit, SiBurpsuite } from 'react-icons/si';
 import { Skill } from '../types';
 
 const iconMap: { [key: string]: JSX.Element } = {
